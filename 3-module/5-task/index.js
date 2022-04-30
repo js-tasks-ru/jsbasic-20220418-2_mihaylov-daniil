b90ca169b3067d32533d6f31b933734396368500
daniyani <daniyanii@yandex.ru>
2022-04-30 15:45:13 +0300

    added 4-module

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,6 +1,6 @@
 function getMinMax(str) {
   const arr = str.split(' ');
-  const arrFilter = arr.filter(item => !isNaN(item) ? true : false)
+  const arrFilter = arr.filter(item => !isNaN(item) ? true : false);
   const arrMap = arrFilter.map(item => Number(item));
   const obj = {
     min: Math.min(...arrMap),
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,16 @@
+const table = document.querySelector('table');
 function makeDiagonalRed(table) {
-  // ваш код...
+  const trs = Array.from(table.rows);
+  for (let i = 0; i < trs.length; i++) {
+    console.log(i);
+    for (let k = 0; k < trs[i].children.length; k++) {
+      if (trs[i].children[k] === trs[i].children[i]) {
+        trs[i].children[k].style.backgroundColor = 'red';
+      }
+    }
+
+  }
+
 }
+
+
