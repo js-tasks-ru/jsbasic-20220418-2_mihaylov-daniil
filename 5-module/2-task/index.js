3de38e5f14fa8b9b1cb5dac361f497543ee8d50b
daniyani <daniyanii@yandex.ru>
2022-05-13 14:17:30 +0300

    added 5-module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector('.toggle-text-button');
+  const text = document.querySelector('#text');
+  btn.addEventListener('click', () => {
+    if (text.hidden !== true) {
+      return text.hidden = true;
+    }
+    return text.hidden = false;
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,55 @@
 function initCarousel() {
-  // ваш код...
+  const carouselArrow = [...document.querySelectorAll('.carousel__arrow')];
+  const inner = document.querySelector('.carousel__inner');
+  let transform = 0;
+  let count = 0;
+  let left;
+  let right;
+  carouselArrow.forEach(arrow => {
+    if (arrow.classList.contains('carousel__arrow_right')) {
+      right = arrow;
+    } else {
+      left = arrow;
+    }
+  });
+
+  right.addEventListener('click', () => {
+    transform += inner.offsetWidth;
+    inner.style.transform = `translateX(-${transform}px)`;
+  });
+
+  right.addEventListener('click', () => {
+    count++;
+    if (count > 0) {
+      left.style.display = '';
+    }
+    if (count === 3) {
+      right.style.display = 'none';
+    }
+
+  });
+
+  left.style.display = 'none';
+  left.addEventListener('click', () => {
+    transform -= inner.offsetWidth;
+    inner.style.transform = `translateX(-${transform}px)`;
+  });
+
+  left.addEventListener('click', () => {
+    count--;
+    if (count < 3) {
+      right.style.display = '';
+    }
+    if (count === 0) {
+      left.style.display = 'none';
+    }
+  });
 }
+
+
+
+
+
+
+
+
