509df40e1af470d7127e1ace7a39fb5208cd55c8
daniyani <daniyanii@yandex.ru>
2022-05-28 03:02:10 +0300

    added 8-module

--- 8-module/4-task/index.js
+++ 8-module/4-task/index.js
@@ -13,33 +13,33 @@ export default class Cart {
   }
 
   addProduct(product) {
-    if(product === null || !product) {
+    if (product === null || !product) {
       return;
     }
-      let cartItem = this.cartItems.find(item => {
-        return item.product === product;
-      })
-      if(cartItem) {
-        cartItem.count++;
-      } else {
-        this.cartItems.push({product, count: 1});
-      }
+    let cartItem = this.cartItems.find(item => {
+      return item.product === product;
+    })
+    if (cartItem) {
+      cartItem.count++;
+    } else {
+      this.cartItems.push({ product, count: 1 });
+    }
 
-      if(this.cartItems.length < 1) {
-        this.cartItems.push({product, count: 1});
-   
-      } 
+    if (this.cartItems.length < 1) {
+      this.cartItems.push({ product, count: 1 });
 
-      this.onProductUpdate(cartItem);
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
     let cartItem = null;
-    for(let i = 0; i < this.cartItems.length; i++) {
-      if(this.cartItems[i].product.id === productId) {
+    for (let i = 0; i < this.cartItems.length; i++) {
+      if (this.cartItems[i].product.id === productId) {
         this.cartItems[i].count += amount;
         cartItem = this.cartItems[i];
-        if(this.cartItems[i].count < 1) {
+        if (this.cartItems[i].count < 1) {
           this.cartItems.splice(i, 1);
         }
       }
@@ -49,7 +49,7 @@ export default class Cart {
   }
 
   isEmpty() {
-    if(this.cartItems.length < 1) {
+    if (this.cartItems.length < 1) {
       return true;
     }
     return false;
@@ -57,7 +57,7 @@ export default class Cart {
 
   getTotalCount() {
     let totalCount = 0;
-    for(let i = 0; i < this.cartItems.length; i++) {
+    for (let i = 0; i < this.cartItems.length; i++) {
       totalCount += this.cartItems[i].count;
     }
     return totalCount;
@@ -65,7 +65,7 @@ export default class Cart {
 
   getTotalPrice() {
     let totalPrice = 0;
-    for(let i = 0; i < this.cartItems.length; i++) {
+    for (let i = 0; i < this.cartItems.length; i++) {
       totalPrice += this.cartItems[i].count * this.cartItems[i].product.price;
     }
     return totalPrice
@@ -73,9 +73,8 @@ export default class Cart {
 
   renderProduct(product, count) {
     return createElement(`
-    <div class="cart-product" data-product-id="${
-      product.id
-    }">
+    <div class="cart-product" data-product-id="${product.id
+      }">
       <div class="cart-product__img">
         <img src="/assets/images/products/${product.image}" alt="product">
       </div>
@@ -113,8 +112,8 @@ export default class Cart {
           <div class="cart-buttons__info">
             <span class="cart-buttons__info-text">total</span>
             <span class="cart-buttons__info-price">€${this.getTotalPrice().toFixed(
-              2
-            )}</span>
+      2
+    )}</span>
           </div>
           <button type="submit" class="cart-buttons__button btn-group__button button">order</button>
         </div>
@@ -123,7 +122,7 @@ export default class Cart {
   }
 
   renderModal() {
-   const modal = new Modal();
+    const modal = new Modal();
     modal.open();
     modal.setTitle('Your order');
     const wrapper = document.createElement('div');
@@ -142,44 +141,44 @@ export default class Cart {
   }
 
   onPlusProduct = (event) => {
-    if(event.target.closest('button').classList.contains('cart-counter__button_plus')) {
+    if (event.target.closest('button').classList.contains('cart-counter__button_plus')) {
       let product = event.target.closest('[data-product-id]');
       let productId = product.dataset.productId;
       this.updateProductCount(productId, 1);
 
-    } 
+    }
   }
 
   onMinusProduct = (event) => {
-    if(event.target.closest('button').classList.contains('cart-counter__button_minus')) {
+    if (event.target.closest('button').classList.contains('cart-counter__button_minus')) {
       let product = event.target.closest('[data-product-id]');
- 
+
       let productId = product.dataset.productId;
       this.updateProductCount(productId, -1);
 
-    } 
-    
+    }
+
   }
 
   onProductUpdate(cartItem) {
-   if(document.querySelector('body').classList.contains('is-modal-open')) {
-     console.log(cartItem);
-    let productId = cartItem.product.id;
-    let modalBody = document.querySelector('.modal__body');
-    let productCount = modalBody.querySelector(`[data-product-id="${productId}"] .cart-counter__count`);
-    let productPrice = modalBody.querySelector(`[data-product-id="${productId}"] .cart-product__price`);
-    let infoPrice = modalBody.querySelector(`.cart-buttons__info-price`);
-    productCount.innerHTML = cartItem.count;
-    productPrice.innerHTML = `€${(cartItem.product.price * cartItem.count).toFixed((2))}`
-    infoPrice.innerHTML = `€${this.getTotalPrice().toFixed((2))}`;
-    if(productCount.innerHTML < 1) {
-      modalBody.querySelector(`[data-product-id="${productId}"]`).remove();
+    if (document.querySelector('body').classList.contains('is-modal-open')) {
+      console.log(cartItem);
+      let productId = cartItem.product.id;
+      let modalBody = document.querySelector('.modal__body');
+      let productCount = modalBody.querySelector(`[data-product-id="${productId}"] .cart-counter__count`);
+      let productPrice = modalBody.querySelector(`[data-product-id="${productId}"] .cart-product__price`);
+      let infoPrice = modalBody.querySelector(`.cart-buttons__info-price`);
+      productCount.innerHTML = cartItem.count;
+      productPrice.innerHTML = `€${(cartItem.product.price * cartItem.count).toFixed((2))}`
+      infoPrice.innerHTML = `€${this.getTotalPrice().toFixed((2))}`;
+      if (productCount.innerHTML < 1) {
+        modalBody.querySelector(`[data-product-id="${productId}"]`).remove();
+      }
+      if (this.isEmpty() === true) {
+        document.querySelector('body').classList.remove('is-modal-open');
+        document.querySelector('.modal').remove();
+      }
     }
-    if(this.isEmpty() === true) {
-      document.querySelector('body').classList.remove('is-modal-open');
-      document.querySelector('.modal').remove();
-    }
-   }
 
     this.cartIcon.update(this);
   }
@@ -187,20 +186,20 @@ export default class Cart {
 
 
   onSubmit(event) {
-   event.preventDefault();
-   let modalBody = document.querySelector('.modal__body');
-   modalBody.querySelector('button[type="submit"]').classList.add('is-loading');
-  const form = modalBody.querySelector('.cart-form');
-  const formData = new FormData(form);
-  console.log(formData);
-  const response = fetch('https://httpbin.org/post', {
+    event.preventDefault();
+    let modalBody = document.querySelector('.modal__body');
+    modalBody.querySelector('button[type="submit"]').classList.add('is-loading');
+    const form = modalBody.querySelector('.cart-form');
+    const formData = new FormData(form);
+    console.log(formData);
+    const response = fetch('https://httpbin.org/post', {
       method: 'POST',
       body: formData,
     })
 
     response
       .then(data => {
-        if(data.status === 200) {
+        if (data.status === 200) {
           document.querySelector('.modal__title').innerHTML = 'Success!';
           this.cartItems.length = 0;
           document.querySelector('.modal__body').innerHTML = `
