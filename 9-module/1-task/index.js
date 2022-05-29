25219ab1b71e61fe84eabfb47f8d8e881d480c84
daniyani <daniyanii@yandex.ru>
2022-05-29 19:54:22 +0300

    fixed 1 task in 9-module

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,12 +1,10 @@
 export default function promiseClick(button) {
-  button.addEventListener('click', event => {
-   new Promise((resolve, reject) => {
-      return resolve(event);
-    })
-  }, {once: true})
+ return new Promise((resolve, reject) => {
+   button.addEventListener('click', event => {
+     resolve(event);
+   }, { once: true });
+ })
 }
 
-promiseClick(button)
-  .then(event => {
-    return console.log(event);
-  })
+
+
